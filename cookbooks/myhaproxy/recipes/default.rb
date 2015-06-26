b7e73769360d1cb1eb859d8dbda41bfca23bee91
Nathen Harvey <nharvey@chef.io>
2015-06-26 12:34:17 -0400

    update the myhaproxy with my backend nodes

--- cookbooks/myhaproxy/recipes/default.rb
+++ cookbooks/myhaproxy/recipes/default.rb
@@ -3,3 +3,20 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+node.default['haproxy']['members'] = [
+  {
+    "hostname" => "ec2-54-208-135-158.compute-1.amazonaws.com",
+    "ipaddress" => "54.208.135.158",
+    "port" => 80,
+    "ssl_port" => 80
+  },
+  {
+    "hostname" => "ec2-54-208-138-164.compute-1.amazonaws.com",
+    "ipaddress" => "54.208.138.164",
+    "port" => 80,
+    "ssl_port" => 80
+  }
+]
+
+
+include_recipe "haproxy::default"

ca10f60da07d3260d56abc400505a39bd478e154
Nathen Harvey <nharvey@chef.io>
2015-06-26 14:15:05 -0400

    Create some environments
    * produciton
    * union

---
+++ cookbooks/myhaproxy/environments/production.rb
@@ -0,0 +1,6 @@
+name "production"
+description "Where we run production code"
+
+cookbook "apache", "= 0.2.1"
+cookbook "myhaproxy", "= 0.4.0"
+
---
+++ cookbooks/myhaproxy/environments/union.rb
@@ -0,0 +1,3 @@
+name "union"
+description "Where we run all new code"
+
